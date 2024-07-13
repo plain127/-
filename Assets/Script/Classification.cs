75aeefa56ef01591d5122a30872af9dc47990aa2
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-13 21:04:53 +0900

    Update Unity Client
    Feat : Update Unity client environment
    
    Model apply and making UI/UX in Unity Environment

---
+++ Assets/Script/Classification.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+
+public class Classification : MonoBehaviour
+{
+
+}
